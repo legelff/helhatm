b0032ef8eca616d5361fb4b6d0ec13839baaff83
L145 <147063982+legelff@users.noreply.github.com>
2025-02-20 15:40:28 +0100

    finished code with data loaded in from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+let { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+let count = [];
+
+prisma.film.groupBy({
+  by: ['rating'],
+  _count: {
+    rating: true
+  }
+}) .then ((result) => {
+  count = result;
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Beacon' });
+  res.render('index', { title: 'The Beacon', name: "test", films: count});
 });
+
+// router.post('/login', function(req, res, res) {
+//   res.render('index', {title: "login page"});
+// })
 
 module.exports = router;
